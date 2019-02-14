6e03bc5303c2df46a236de9295961210efb6c493
Gyulavári Peti <e137projects@gmail.com>
2019-02-14 20:59:04 +0100

    progress bar placement fix

--- CoffeTime/app/src/main/java/com/example/x/coffeetime/application/api/ResponseModel/MessageResponses.kt
+++
@@ -1,6 +0,0 @@
-package com.example.x.coffeetime.application.api.ResponseModel
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class loginResponse(val error: String,  val error_description: String)
